344f739e01872d33bfa2daed6dd46b5b74892da5
Nishant Parmar <53419956+nishantparmar24@users.noreply.github.com>
2020-02-19 11:34:47 +0530

    Implementation of Minesweeper Challenge in Python

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import util
 
 
-class MineNode:
-
-    def __init__(self):
-        self.value = 0
-        self.x = 0
-        self.y = 0
-
-    def create_node(self, x, y, value=0):
-        self.x = x
-        self.y = y
-        self.value = value
-
-    def __repr__(self):
-        return "Node: ({}, {}) <{}>".format(self.x, self.y, self.value)
-
-
 class BattleGround:
+
+    class MineNode:
+
+        def __init__(self):
+            self.value = 0
+            self.x = 0
+            self.y = 0
+
+        def create_node(self, x, y, value=0):
+            self.x = x
+            self.y = y
+            self.value = value
+
+        def __repr__(self):
+            return "Node: ({}, {}) <{}>".format(self.x, self.y, self.value)
 
     def __init__(self, n=8):
         self.grid = list()
         self.side = n
-        self.move = 0
+        self.moves = list()
         self.status = 0
 
     def initialize_grid(self):
@@ -31,21 +30,32 @@ class BattleGround:
             row = list()
             for j in range(self.side):
                 # print("\ni={}\tj={}".format(i, j))
-                new_node = MineNode()
+                new_node = self.MineNode()
                 new_node.create_node(i, j)
                 row.append(new_node)
             self.grid.append(row)
 
     def deploy_mines(self):
         mines = util.define_mines(self.side)
-        print(mines)
+        print("mines are deployed at: {}".format(mines))
+        neighbors = list()
         for mine in mines:
             x, y = mine
             self.grid[x][y].value = "m"
+            neighbors.extend(util.get_neighbours(mine, self.side))
+        print(neighbors)
+        for node in neighbors:
+            r, c = node
+            node_value = self.grid[r][c].value
+            print("Neighbor node: {} <{}>".format(node, node_value))
+            # print()
+            if isinstance(node_value, int):
+                self.grid[r][c].value += 1
 
     def display(self):
-        if self.move == 0:
+        if len(self.moves) == 0:
             util.print_grid_skeleton(self.side)
+
 
 
 if __name__ == "__main__":
@@ -54,3 +64,4 @@ if __name__ == "__main__":
     new_ground.initialize_grid()
     new_ground.deploy_mines()
     print(new_ground.grid)
+    # util.get_neighbours((7, 7))
--- util.py
+++ util.py
@@ -21,3 +21,15 @@ def define_mines(n=8):
         if mine_pos not in mines:
             mines.append(mine_pos)
     return mines
+
+
+def get_neighbours(node, n=8):
+    x, y = node
+    values = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y),
+              (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
+    value_combos = []
+    for value in values:
+        xn, yn = value
+        if xn in range(n) and yn in range(n):
+            value_combos.append(value)
+    return value_combos
